95296babb543aae83b7a218a2e925e6e0d84a42c
Tobias Kolditz <tbs.kldtz@gmail.com>
2019-07-09 09:54:14 +0200

    Simplify token overlap

--- bratiaa/utils.py
+++ bratiaa/utils.py
@@ -1,4 +1,5 @@
 import re
+
 import numpy as np
 
 ENCODING = 'utf-8'
@@ -28,26 +29,25 @@ class TokenOverlap:
 
     @staticmethod
     def compute_mapping(text_length, tokens):
-        char2token = np.zeros(text_length + 1, dtype=int)
+        char2token = np.zeros(text_length, dtype=int)
         i = 0
         for token_idx, (start, end) in enumerate(tokens):
             char2token[i:start] = token_idx - 1
             char2token[start:end] = token_idx
             i = end
-        char2token[i:text_length + 1] = len(tokens) - 1
+        char2token[i:text_length] = len(tokens) - 1
         return char2token
 
     def overlapping_tokens(self, start, end):
-        start_token = self.char2token[start]
-        if start_token == -1:
-            start_token = 0
-        if self.tokens[start_token][1] <= start:
-            start_token += 1
-        end_token = self.char2token[end]
-        if end_token < 0:
+        assert end <= len(self.char2token), f'End index {end} > text length {len(self.char2token)}!'
+        if end < 1 or start >= end:
             return []
-        if self.tokens[end_token][0] >= end:
-            end_token -= 1
-        if end_token < start_token:
+        start_token = self.char2token[start]
+        if start_token == -1: # start offset before first token
+            start_token = 0
+        if self.tokens[start_token][1] <= start: # start offset between two tokens
+            start_token += 1
+        end_token = self.char2token[end - 1] # end offset is exclusive
+        if end_token < 0 or end_token < start_token:
             return []
         return self.tokens[start_token:end_token + 1]
