882d255e9d40fb908e9927442f884cc5ae68a4c9
Siim Orasmaa <siim.orasmaa@ut.ee>
2020-09-11 17:12:56 +0300

    Updated requirements: Py 3.6+ is required because f-strings are used

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='bratiaa',
           'scipy',
           'tabulate'
       ],
-      python_requires='>=3.5',
+      python_requires='>=3.6',
       entry_points={
           'console_scripts': [
               'brat-iaa=bratiaa.agree_cli:main'
